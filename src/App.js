133ac9b1b065abd4899d3ad25a6832d51fb7b3d3
tjfgk97 <tjfgk97@gmail.com>
2024-09-07 02:48:08 +0900

    How to use react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      
+      <Button variant="success">Success</Button>{' '}
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
